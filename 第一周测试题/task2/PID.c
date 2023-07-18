e65254cb283c2807d358ba839d6029d9b7ee1d47
杨凯博 <aliykb@outlook.com>
2023-07-18 10:25:53 +0800

    第一周测试题

--- 第一周测试题/task2/PID.c
+++ 第一周测试题/task2/PID.c
@@ -14,8 +14,6 @@
 
 static void Calculate(PID_t *pid, float current_value, float target_value);
 
-PID_t pid;
-
 /** @brief  结构体格式初始化，为输入参数结构体中的函数指针变量赋值，并初始化一些属性
   * @param  pid            : 被操作的结构体，可理解为self
             p              : 给定的p
@@ -26,23 +24,20 @@ PID_t pid;
 */
 void PID_StructInit(PID_t *pid, float p, float i, float d, float integral_limit, float max_output)
 {
-  // 初始化相关变量
-  pid->TargetValue = 0.0f;
-  pid->CurrentValue = 0.0f;
-  pid->Error = 0.0f;
-  pid->LastError = 0.0f;
-  pid->PValue = 0.0f;
-  pid->IValue = 0.0f;
-  pid->DValue = 0.0f;
-  pid->output = 0.0f;
-  // 设置PID参数
-  pid->KP = p;
-  pid->KI = i;
-  pid->KD = d;
-  pid->integral_limit = integral_limit;
-  pid->max_output = max_output;
-  // 调用函数指针
-  pid->f_Calculate = Calculate;
+  /***
+   * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+   * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+   * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+   * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+   * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+   * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+   * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+   * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+   * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+   * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+   * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+   * Hello, please delete me and coding here~
+   */
 }
 
 /** @brief  私有函数，但可以结构体内函数指针的方式被外部调用。功能为计算一帧的pid
@@ -52,29 +47,18 @@ void PID_StructInit(PID_t *pid, float p, float i, float d, float integral_limit,
 */
 static void Calculate(PID_t *pid, float current_value, float target_value)
 {
-  // 更新目标值和当前值
-  pid->TargetValue = target_value;
-  pid->CurrentValue = current_value;
-  // 计算误差
-  pid->Error = pid->TargetValue - pid->CurrentValue;
-  // 计算比例项
-  pid->PValue = pid->KP * pid->Error;
-  // 计算积分项
-  pid->IValue += pid->KI * pid->Error;
-  // 抗积分饱和限制
-  if (pid->IValue > pid->integral_limit)
-    pid->IValue = pid->integral_limit;
-  else if (pid->IValue < -pid->integral_limit)
-    pid->IValue = -pid->integral_limit;
-  // 计算微分项
-  pid->DValue = pid->KD * (pid->Error - pid->LastError);
-  // 保存上次误差
-  pid->LastError = pid->Error;
-  // 计算输出
-  pid->output = pid->PValue + pid->IValue + pid->DValue;
-  // 输出限制
-  if (pid->output > pid->max_output)
-    pid->output = pid->max_output;
-  else if (pid->output < -pid->max_output)
-    pid->output = -pid->max_output;
+  /***
+   * ░░░░░░░░░░░░░░░░░░░░░░░░▄░░
+   * ░░░░░░░░░▐█░░░░░░░░░░░▄▀▒▌░
+   * ░░░░░░░░▐▀▒█░░░░░░░░▄▀▒▒▒▐
+   * ░░░░░░░▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐
+   * ░░░░░▄▄▀▒░▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐
+   * ░░░▄▀▒▒▒░░░▒▒▒░░░▒▒▒▀██▀▒▌
+   * ░░▐▒▒▒▄▄▒▒▒▒░░░▒▒▒▒▒▒▒▀▄▒▒
+   * ░░▌░░▌█▀▒▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐
+   * ░▐░░░▒▒▒▒▒▒▒▒▌██▀▒▒░░░▒▒▒▀▄
+   * ░▌░▒▄██▄▒▒▒▒▒▒▒▒▒░░░░░░▒▒▒▒
+   * ▀▒▀▐▄█▄█▌▄░▀▒▒░░░░░░░░░░▒▒▒
+   * Hello, please delete me and coding here~
+   */
 }
